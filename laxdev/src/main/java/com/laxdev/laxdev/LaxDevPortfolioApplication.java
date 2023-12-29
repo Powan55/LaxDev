15f7b5fb111e42b357b42b203b2089b80edebb2d
Laxmi Poudel <76706653+Powan55@users.noreply.github.com>
2023-12-29 03:53:49 -0500

    Add files via upload

---
+++ laxdev/src/main/java/com/laxdev/laxdev/LaxDevPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.laxdev.laxdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaxDevPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaxDevPortfolioApplication.class, args);
+	}
+
+}
---
+++ laxdev/src/test/java/com/laxdev/laxdev/LaxDevPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laxdev.laxdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaxDevPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
